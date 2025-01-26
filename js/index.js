2f0600ddc23b9a73a67a1b73bc1a9b4bc511c502
lzx20129002 <lzx20129002@163.com>
2025-01-26 17:52:47 +0800

    改目录结构

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function load_html(file_name) {
             const $html = $(data);
             const $body = $html.filter('#body');
             // 或者，直接插入整个 HTML 内容
-            $('#body').html($body);
+            $('#body').html($body.html());
         },
         error: function(xhr, status, error) {
             $('#body').html(status+'<br>'+error);
